e4c75e750d8b4e75925e3fce22beaafc146d7339
ZhukovAlV <Olenka2012$github>
2021-05-05 17:11:52 +0600

    Update UserServiceStub.java (update method Save)

--- demo/src/main/java/com/example/demo/service/UserServiceStub.java
+++ demo/src/main/java/com/example/demo/service/UserServiceStub.java
@@ -1,6 +1,8 @@
 package com.example.demo.service;
 
 import com.example.demo.dto.UserDto;
+import com.example.demo.entity.EmailAddress;
+import com.example.demo.entity.EmailContent;
 import com.example.demo.entity.Message;
 import com.example.demo.entity.User;
 import com.example.demo.repository.UserRepository;
@@ -15,6 +17,7 @@ import java.util.concurrent.TimeoutException;
 import java.util.stream.Collectors;
 
 import static java.util.Objects.isNull;
+import static java.util.Objects.nonNull;
 
 @Service
 @AllArgsConstructor
@@ -22,15 +25,27 @@ public class UserServiceStub implements UserService {
     private final UserRepository userRepository;
     private final UserConverter userConverter;
     private final MessagingServiceStub messagingService;
+    private final SendMailerStub sendMailerStub;
 
     @Override
     @Transactional
     public UserDto saveUser(@NotNull UserDto userDto) throws ValidationException, TimeoutException {
+        // Проверяем введенные данные на корректность
         validateUserDto(userDto);
-        User convertedUser = userConverter.fromUserDtoToUser(userDto);
-        User savedUser = userRepository.save(convertedUser);
-        messagingService.doRequest(new Message<>(userDto));
-        return userConverter.fromUserToUserDto(savedUser);
+
+        // Отправляем данные во внешнюю систему на проверку и ждем ответа
+        if (nonNull(messagingService.doRequest(new Message<>(userDto)))) {
+            // Если все успешно создаем пользователя и сохраняем в БД
+            User convertedUser = userConverter.fromUserDtoToUser(userDto);
+            User savedUser = userRepository.save(convertedUser);
+            // И отправляем письмо пользователю
+            EmailAddress toAddress = new EmailAddress(userDto.getEmail());
+            EmailContent messageBody = new EmailContent("Поздравляем с успешной регистрацией");
+            sendMailerStub.sendMail(toAddress, messageBody);
+            return userConverter.fromUserToUserDto(savedUser);
+        }
+        // В случае не успешной проверки ничего не возращаем
+        return null;
     }
 
     @Override
@@ -61,6 +76,18 @@ public class UserServiceStub implements UserService {
         }
         if (isNull(userDto.getLogin()) || userDto.getLogin().isEmpty()) {
             throw new ValidationException("Login is empty");
+        }
+        if (nonNull(userRepository.findByLogin(userDto.getLogin()))) {
+            throw new ValidationException("Login already exists");
+        }
+        if (isNull(userDto.getName()) || isNull(userDto.getSurname())) {
+            throw new ValidationException("Name or Surname is empty");
+        }
+        if (isNull(userDto.getEmail())) {
+            throw new ValidationException("Email is empty");
+        }
+        if (!userDto.getEmail().matches("[\\w\\s\\d]+@([\\w\\s\\d\\u002E])((ru)|(com))")) {
+            throw new ValidationException("Email is not correct");
         }
     }
 }
